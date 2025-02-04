dab9a2f252e149a89aac59d47f88f841e1819d7c
warriornub <cmpqwdoq@gmail.com>
2025-02-04 14:40:25 +0800

    a

--- SERP.Infrastructure/Migrations/20250204063607_Group_GroupCode_GroupType.Designer.cs
+++ SERP.Infrastructure/Migrations/20250204063607_Group_GroupCode_GroupType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SERP.Infrastructure.Common.DBContexts;
 
@@ -11,9 +12,11 @@ using SERP.Infrastructure.Common.DBContexts;
 namespace SERP.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250204063607_Group_GroupCode_GroupType")]
+    partial class Group_GroupCode_GroupType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SERP.Infrastructure/Migrations/20250204063607_Group_GroupCode_GroupType.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SERP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Group_GroupCode_GroupType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Group_group_code",
+                table: "Group");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Group_group_code_group_type",
+                table: "Group",
+                columns: new[] { "group_code", "group_type" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Group_group_code_group_type",
+                table: "Group");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Group_group_code",
+                table: "Group",
+                column: "group_code",
+                unique: true);
+        }
+    }
+}
